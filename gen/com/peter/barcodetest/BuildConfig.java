9f5e6ef54efc46f2e9a99e22e66ed09eab9ac2d1
petertbernhardt <petertbernhardt@gmail.com>
2014-01-15 16:45:37 -0800

    Initial commit

---
+++ gen/com/peter/barcodetest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.peter.barcodetest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/peter/barcodetest/Barcode.java
@@ -0,0 +1,63 @@
+package com.peter.barcodetest;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+@SuppressLint("NewApi")
+public class Barcode extends Activity implements OnClickListener{
+	
+	private Button scanBtn;
+	private TextView formatTxt, contentTxt;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_barcode);
+		scanBtn = (Button)findViewById(R.id.scan_button);
+		formatTxt = (TextView)findViewById(R.id.scan_format);
+		contentTxt = (TextView)findViewById(R.id.scan_content);
+		scanBtn.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.barcode, menu);
+		return true;
+	}
+	
+	public void onClick(View v){
+		//respond to clicks
+		if(v.getId()==R.id.scan_button){
+			//scan
+			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+			scanIntegrator.initiateScan();
+		}
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		//retrieve scan result
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if (scanningResult != null) {
+			//we have a result
+			String scanResult = scanningResult.getContents();
+			String scanFormat = scanningResult.getFormatName();
+			formatTxt.setText("FORMAT: " + scanFormat);
+			contentTxt.setText("CONTENT: " + scanResult);
+		} else {
+			Toast toast = Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT);
+			toast.show();
+		}
+	}
+	
+}
